adb7609e0e504302b5296ceb0c66deb30733744f
katehg <e.delyagina@gmail.com>
2017-03-30 20:02:27 +0300

    Add files via upload

---
+++ grammy.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, jsonify
+import requests
+
+app = Flask(__name__)
+app.debug = True
+
+def get_data():
+    url = "https://raw.githubusercontent.com/katehg/delyagina_kate/master/grammy_new.json"
+    r = requests.get(url)
+    return r.json()
+
+
+@app.route('/')
+def list_rows():
+    our_data = get_data()
+    return render_template('grammy_1.html', data=our_data)
+
+@app.route('/sort_colors')
+def list_rows():
+    our_data = get_data()
+    return render_template('grammy_2.html', data=our_data)
+
+if __name__ == '__main__':
+    app.run()
+
